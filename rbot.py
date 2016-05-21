3a4a2c6b9d14734a1025ba8b71dfe4244905589a
John Kulp <john.h.kulp@gmail.com>
2016-05-21 15:00:48 -0400

    arrrrgh

--- rbot.py
+++ rbot.py
@@ -21,7 +21,7 @@ def route_create_list():
 
 @app.route("/highfive", methods=['POST'])
 def route_highfive():
-	return "args: " + str(request.args.get('user_name') + " " + str(request.args.get('text')))
+	return "args: " + str(request.args.get('user_name') + " " + str(request.args.get('text'))
 	result = {}
 	result.text = "@" + request.args.get('user_name') + \
 		' sent a high five to ' + request.args.get('text')
