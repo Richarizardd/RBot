16a1bb2407f0b6cab5b1300d462e130ca84c8499
John Kulp <john.h.kulp@gmail.com>
2016-05-21 14:29:57 -0400

    adds test high five command

--- rbot.py
+++ rbot.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,3 +9,14 @@ def send_intro_text():
 		Thanks for using rBot!
 		To get started, type /help for a list of commands
 	"""
+
+@app.route("/create_list")
+def route_create_list():
+	return "ok"
+
+@app.route("/highfive")
+def route_highfive():
+	result = {}
+	result.text = "@" + request.args.get('user_name') + 
+		' sent a high five to ' + request.args.get('text')
+	return result
