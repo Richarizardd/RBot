352819a7ecf098efc9f8199b36ad847b34fac48c
John Kulp <john.h.kulp@gmail.com>
2016-05-21 13:32:45 -0400

    adds first route

---
+++ rbot.py
@@ -0,0 +1,9 @@
+from flask import flask
+app = Flask(__name__)
+
+@app.route("/")
+def send_intro_text():
+	return """"
+		Thanks for using rBot!
+		To get started, type /help for a list of commands
+	"""
