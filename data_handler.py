06c017cd14e523a17c14fca0fc5cac5929a6feef
John Kulp <john.h.kulp@gmail.com>
2016-05-24 15:51:07 -0400

    lots of bug fixing to get the database integrated.

--- data_handler.py
+++ data_handler.py
@@ -5,210 +5,247 @@ import sqlite3
 #runs initial commands to wipe the DB and set up all tables
 def set_up_DB(conn):
 
-    #drop the tables if they already exists
-    #fail gracefully if the tables don't exist yet
-    try:
-        conn.execute('DROP TABLE Users')
-    except Exception:
-        print("Users doesn't exist yet")
-        pass
+	#drop the tables if they already exists
+	#fail gracefully if the tables don't exist yet
 
-    try:
-        conn.execute('DROP TABLE Lists')
-    except Exception:
-        print("Lists doesn't exist yet")
-        pass
+	try:
+		conn.execute('DROP TABLE Users')
+	except Exception:
+		print("Users doesn't exist yet")
 
-    try:
-        conn.execute('DROP TABLE Content')
-    except Exception:
-        print("Content doesn't exist yet")
-        pass
+	try:
+		conn.execute('DROP TABLE Lists')
+	except Exception:
+		print("Lists doesn't exist yet")
 
-    try:
-        conn.execute("""
-            CREATE TABLE Users (
-                UserID int PRIMARY KEY AUTO_INCREMENT,
-                UserName varchar(255)
-            );
-            """)
-    except Exception:
-        print("Cannot create user table")
-        pass
-      
-    try:            
-        conn.execute("""
-            CREATE TABLE Lists (
-                ListID int PRIMARY KEY AUTO_INCREMENT,
-                ListName varchar(255),
-                UserID int,
-                PermissionLevel int
-            );
-            """)
-    except Exception:
-        print("Cannot create lists table")
-        pass
-        
-    try:            
-        conn.execute("""
-            CREATE TABLE Content (
-                ContentID int PRIMARY KEY AUTO_INCREMENT,
-                Title varchar(255),
-                Description text,
-                Category varchar(255),
-                UserID int,
-                URL int,
-            );
-            """)
-    except Exception:
-        print("Cannot create content table")
-        pass
-        
-    try:            
-        conn.execute("""
-            CREATE TABLE List_to_Content (
-                ListID int NOT NULL,
-                ContentID int NOT NULL
-            );
-            """)
-    except Exception:
-        print("Cannot create list_to_content table")
-        pass
-  
- 
-#adds new user        
-def add_user(user_name):
-    
-    #create a new entry in the User table 
+	try:
+		conn.execute('DROP TABLE Content')
+	except Exception:
+		print("Content doesn't exist yet")
+
+	try:
+		conn.execute('DROP TABLE List_to_Content')
+	except Exception:
+		print("List_to_Content doesn't exist yet")
+
+	try:
+		conn.execute('DROP TABLE Tags')
+	except Exception:
+		print("Tags doesn't exist yet")
+
+	try:
+		conn.execute('DROP TABLE Items_Tags')
+	except Exception:
+		print("Items_Tags doesn't exist yet")
+
+	conn.commit()
+
 	try:
 		conn.execute("""
-            INSERT INTO Users (UserName)
-            VALUES (%s)
-            """, user_name)
+			CREATE TABLE Users (
+				UserID INTEGER PRIMARY KEY AUTOINCREMENT,
+				UserName VARCHAR(33)
+			)
+			""")
+	except Exception:
+		print("Cannot create user table")
+	
+	try:   
+		conn.execute("""
+			CREATE TABLE Lists (
+				ListID INTEGER PRIMARY KEY AUTOINCREMENT,
+				ListName VARCHAR(32),
+				UserID INTEGER,
+				PermissionLevel INTEGER
+			)
+		""")
+	except Exception:
+		print("Cannot create lists table")
+		
+	try:            
+		conn.execute("""
+			CREATE TABLE Content (
+				ContentID INTEGER PRIMARY KEY AUTOINCREMENT,
+				Title VARCHAR(32),
+				Description TEXT,
+				Category VARCHAR(32),
+				UserID INTEGER,
+				URL INTEGER
+			)
+		""")
+	except Exception:
+		print("Cannot create content table")
+		
+	try:
+		conn.execute("""
+			CREATE TABLE List_to_Content (
+				ListID INTEGER NOT NULL,
+				ContentID INTEGER NOT NULL
+			)
+		""")
+	except Exception:
+		print("Cannot create list_to_content table")
+		
+	try:
+		conn.execute("""
+			CREATE TABLE Tags(
+				TagID INTEGER PRIMARY KEY NOT NULL,
+				TagTitle INTEGER NOT NULL
+			)
+		""")
+	except Exception:
+		print("Cannot create Tags table")
+		
+	try:
+		conn.execute("""
+			CREATE TABLE Items_Tags(
+				ContentID INTEGER NOT NULL,
+				TagID INTEGER NOT NULL,
+				PRIMARY KEY(ContentID, TagID)
+			)
+		""")
+	except Exception:
+		print("Cannot create Items_Tags table")
+   
+
+
+	conn.commit()
+ 
+#adds new user        
+def add_user(conn, user_name):
+	
+	#create a new entry in the User table 
+	try:
+		conn.execute("""
+			INSERT INTO Users (UserName)
+			VALUES (%s)
+			""", user_name)
 	except Exception:
 		print("Cannot add new user")
 		pass
-        
-        
+		
+		
 #adds new list        
-def add_list(list_name, user_id, permission=0):
-    
-    #create a new entry in the Lists table 
-    #default permission level = 0 (private)
+def add_list(conn, list_name, user_id, permission=0):
+	
+	#create a new entry in the Lists table 
+	#default permission level = 0 (private)
 	try:
 		conn.execute("""
-            INSERT INTO Lists (ListName, UserID, PermissionLevel)
-            VALUES (%s, %d, %d)
-            """, list_name, user_id, permission)
+			INSERT INTO Lists (ListName, UserID, PermissionLevel)
+			VALUES (%s, %s, %s)
+			""", list_name, user_id, permission)
 	except Exception:
 		print("Cannot add new list")
-		pass
  
-    
+	
 #adds new content
-def add_content(title, desc, category, url, user_id, list_id):
-    
-    #create a new entry in the Content table
-    try:
-        conn.execute("""
-        INSERT INTO Content (Title, Description, Category, UserID)
-        VALUES (%s, %s, %s, %d)
-        """, title, desc, category, user_id)
-    except Exception:
-        print("Cannot add new content")
-        pass
+def add_content(conn, title, desc, category, url, user_name, list_id):
+	
+	#check if the user owns the list being added to
+	try:
+		cursor = conn.execute("""
+			SELECT ListID FROM Lists WHERE ListID = %s AND UserID = (
+				SELECT UserID FROM Users WHERE User
+			)
 
-    #create a new list-to-content relationship
-    try:
-        content_id = conn.insert_id()
-        conn.execute("""
-            INSERT INTO List_to_Content (ListID, ContentID)
-            VALUES (%d, %d)
-            """, list_id, content_id)
-    except Exception:
-        print("Cannot create associate content with list")
-        pass
-    
+		""")
+
+
+	#create a new entry in the Content table
+	try:
+		conn.execute("""
+		INSERT INTO Content (Title, Description, Category, UserID)
+		VALUES (%s, %s, %s, %s)
+		""", title, desc, category, user_id)
+	except Exception:
+		print("Cannot add new content")
+
+	#create a new list-to-content relationship
+	try:
+		content_id = conn.insert_id()
+		conn.execute("""
+			INSERT INTO List_to_Content (ListID, ContentID)
+			VALUES (%s, %s)
+			""", list_id, content_id)
+	except Exception:
+		print("Cannot create associate content with list")
+	
 
 #returns information of interest given content ID
-def search_content_by_id(content_id):
+def search_content_by_id(conn, content_id):
    
-    #fetch content title, description, category and user name 
-    try:
-        cursor = conn.execute("SELECT * FROM Contents WHERE ContentID = '%d'" % content_id)
-    except Exception:
-        print("Query to table Contents fails")
-        pass
-     
-    (title, desc, category, user_id) = cursor.fetchone()
-    
-    try:
-        cursor = conn.execute("SELECT UserName FROM Users WHERE UserID = '%d'" % user_id)
-    except Exception:
-        print("Query to table Users fails")
-        pass
-    
-    user_name = cursor.fetchone()[0]
+	#fetch content title, description, category and user name 
+	try:
+		cursor = conn.execute("SELECT * FROM Contents WHERE ContentID = '%s'" % content_id)
+	except Exception as err:
+		print("Query to table Contents fails")
+		return "something went wrong selecting the content!  " + str(err)
+		
+	for row in cursor:
+		(title, desc, category, user_id) = row
 
-    return title, desc, category, user_name
-    
+	return title, desc, category, user_id
+	
 
 #returns information of interest given list name
-def search_contents_by_list_name(list_name):
-    
-    #fetch lists given the list name if the permission level is public
-    try:
-        cursor = conn.execute("SELECT ListID FROM Lists WHERE ListName = '%s'" % list_name)
-    except Exception:
-        print("List name does not exist")
-        pass
-        
-    list_ids = [row[0] for row in cursor]
-    content_ids = []
-    
-    for list_id in list_ids:
-        cursor = conn.execute("SELECT ContentID FROM List_to_Content WHERE ListID = '%d'" % list_id)
-        content_ids += [row[0] for row in cursor]
-        
-    info = []
-    for content_id in content_ids:
-        try:
-            cursor = conn.execute("SELECT * FROM Content WHERE ContentID = '%d'" % content_id)
-            (title, desc, category, user_id) = cursor.fetchone()
-        except Exception:
-            pass
-        try:
-            cursor = conn.execute("SELECT UserName FROM Users WHERE UserID = '%d'" % user_id)
-            user_name = cursor.fetchone()[0]
-            info.append([title, desc, category, user_name])
-        except Exception:
-            pass
-        
-    return info
-    
+def search_contents_by_list_name(conn, list_name):
+	
+	#fetch lists given the list name if the permission level is public
+	try:
+		cursor = conn.execute("SELECT ListID FROM Lists WHERE ListName = '%s'" % list_name)
+	except Exception:
+		print("List name does not exist")
+		pass
+		
+	list_ids = [row[0] for row in cursor]
+	content_ids = []
+	
+	for list_id in list_ids:
+		cursor = conn.execute("SELECT ContentID FROM List_to_Content WHERE ListID = '%s'" % list_id)
+		content_ids += [row[0] for row in cursor]
+		
+	info = []
+	for content_id in content_ids:
+		try:
+			cursor = conn.execute("SELECT * FROM Content WHERE ContentID = '%s'" % content_id)
+			(title, desc, category, user_id) = cursor.fetchone()
+		except Exception:
+			pass
+
+		try:
+			cursor = conn.execute("SELECT UserName FROM Users WHERE UserID = '%s'" % user_id)
+			user_name = cursor.fetchone()[0]
+			info.append([title, desc, category, user_name])
+		except Exception:
+			pass
+		
+	return info
+	
 
 #returns information of interest given user ID
-def search_contents_by_user(user_id):
-    
-    #fetch contents given user ID
-    info = []
-    try:
-        cursor = conn.execute("SELECT * FROM Content WHERE UserID = '%d'" % user_id)
-        for row in cursor:
-            (title, desc, category, user_id) = row
-            # try:
-            #     cursor = conn.execute("SELECT UserName FROM Users WHERE UserID = '%d'" % user_id)
-            #     user_name = cursor.fetchone()[0]
-            #     info.append([title, desc, category, user_name])
-            # except Exception:
-            #     pass
-    except Exception:
-        pass
-    
-    return info
-        
-    
+def search_contents_by_user(conn, user_id):
+	
+	#fetch contents given user ID
+	info = []
+	try:
+		cursor = conn.execute("SELECT * FROM Content WHERE UserID = '%s'" % user_id)
+		for row in cursor:
+			(title, desc, category, user_id) = row
+			# try:
+			#     cursor = conn.execute("SELECT UserName FROM Users WHERE UserID = '%d'" % user_id)
+			#     user_name = cursor.fetchone()[0]
+			#     info.append([title, desc, category, user_name])
+			# except Exception:
+			#     pass
+	except Exception:
+		pass
+	
+	return info
+		
+	
 if __name__ == '__main__':
 	#run dev tests
 	conn = sqlite3.connect('db/research_test.db')
 	set_up_DB(conn)
+	conn.close()
--- rbot.py
+++ rbot.py
@@ -1,10 +1,32 @@
 #!/usr/bin/env python
 from flask import Flask
 from flask import request
+from flask import g
+import sqlite3
 import sys
+import shlex
+import data_handler
 app = Flask(__name__)
 
 import data_handler
+app.debug = True
+
+# open a db connection and have it fail gracefully on exitdef get_db():
+DATABASE = 'db/research_test.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect('db/research_test.db')
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
 
 @app.route("/")
 def send_intro_text():
@@ -23,39 +45,73 @@ def route_create_list():
 
 @app.route("/highfive", methods=['POST'])
 def route_highfive():
-	print( str(request.args))
-	result = "args: " + str(request.form['user_name']) + " " + str(request.form['text'])
+	result = "@" + str(request.form['user_name']) + \
+		' sent a high five to ' + str(request.form['text'])
+	return result
+
+#adds a list to the user's lists
+@app.route("/create_list", methods=['POST'])
+def route_add_list():
+	args = []
+
+	lexer = shlex.shlex(request.form['text'])
+	for token in lexer:
+		args.append(token)
+
+	if len(args < 2):
+		return "please use the format:\n/r+list <list_name> \"<description>\""
+
+
+	lname = args[1]
+	descr = args[2]
+
+
+
+
+@app.route("/add_article", methods=['POST'])
+def route_add_article():
+
+	args = []
+
+	lexer = shlex.shlex(request.form['text'])
+	for token in lexer:
+		args.append(token)
+
+	if len(args) < 5:
+		return "please use the format:\n/radd <list> <title> <description> \"<url>\" \"<string separated category tags>\""
+
+
+	list_name = args[0]
+	title = args[1]
+	description = args[2][1:len(args[2])-1]
+	url = args[3]
+	#array deliminated by spaces
+	tags = args[4][1:len(args[4])-1].split(' ')
+
+	user_name = request.form['user_name']
+
+	data_handler.add_content(get_db(), title, description, tags, url, 1, 1)
+
+	return "added! title: " + str(title) + ", description: " + str(description) + ", tags: " + str(tags) + ", url: " + str(url)
+
+
+@app.route("/get_article", methods=['POST'])
+def route_get_article():
+	arg = request.form['text']
+
+	#get the article with that ID value
+	result = data_handler.search_content_by_id(get_db(), request.form['text'])
+
 	return result
 	# result = {}
 	# result.text = "@" + str(request.args.get('user_name')) + \
 	# 	' sent a high five to ' + str(request.args.get('text'))
 	# return result
 
-@app.route("/add_article", methods=['POST'])
-def route_add_article():
-	args = request.form['text'].split(' ')
-	if len(args) < 3:
-		return "please use the format /radd <title> <url> <description>"
-	title = args[0]
-	url = args[1]
-	description = ' '.join(args[2:])
 
-	data_handler.add_content(title, description, 'stuff', url, 1, 1)
 
-	return "added!"
-	# result = {}
-	# result.text = "@" + str(request.args.get('user_name')) + \
-	# 	' sent a high five to ' + str(request.args.get('text'))
-	# return result
 
-@app.route("/get_article", methods=['GET'])
-def route_get_article():
-	arg = request.form['text']
-	return search_content_by_id(request.form['text'])
-	# result = {}
-	# result.text = "@" + str(request.args.get('user_name')) + \
-	# 	' sent a high five to ' + str(request.args.get('text'))
-	# return result
+
 
 if __name__ == "__main__":
     app.run()
